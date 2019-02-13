313662b5f5faff6f64a5d1fd2a061ce013a2de62
jcharbo <jcharbo@users.noreply.github.com>
2019-02-13 17:54:29 -0600

    Create resizedImageClass.java

---
+++ android/src/main/java/fr/bamlab/rnimageresizer/resizedImageClass.java
@@ -0,0 +1,15 @@
+package fr.bamlab.rnimageresizer;
+
+import java.io.File;
+
+public class resizedImageClass {
+    File resized;
+    float height;
+    float width;
+    resizedImageClass(File r,float h,float w)
+    {
+        resized = r;
+        height = h;
+        width = w;
+    }
+}
